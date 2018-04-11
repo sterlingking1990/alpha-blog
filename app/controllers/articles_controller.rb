d37d2deebba32a35b94d77bbd06bb8c098349a90
Rails Udemy Course <izundukingsleyemeka@gmail.com>
2018-04-11 10:44:51 -1100

    Made changes on alpha-blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-	
+	before_action :set_article, only: [:edit, :update, :destroy, :show]
 
 	def index
 		@articles=Article.all
@@ -28,12 +28,12 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article=Article.find(params[:id])
+		
 
 	end
 
 	def destroy
-		@article= Article.find(params[:id])
+		
 		@article.destroy
 		flash[:notie]="Article successfully Deleted"
 		redirect_to articles_path
@@ -44,11 +44,11 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		
 	end
 
 	def update
-		@article=Article.find(params[:id])
+		
 		if @article.update(article_params)
 			flash[:notice]="Article was updated successfully"
 			redirect_to article_path(@article)
@@ -58,6 +58,9 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+		def set_article
+			@article= Article.find(params[:id])
+		end
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
